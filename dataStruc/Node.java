e1ef9a0216131f75b35c76a60f29f8f89b102447
cookAndDrum <99819755+cookAndDrum@users.noreply.github.com>
2023-02-18 15:04:01 +0800

    First commit

---
+++ dataStruc/Node.java
@@ -0,0 +1,25 @@
+public class Node {
+  private String element;
+  private Node next;
+
+  public Node(String s, Node n) {
+    element = s;
+    next = n;
+  }
+
+  public String getElement() {
+    return element;
+  }
+
+  public Node getNext() {
+    return next;
+  }
+
+  public void setElement(String newE) {
+    element = newE;
+  }
+
+  public void setNext(Node newN) {
+    next = newN;
+  }
+}
---
+++ dataStruc/SLinkedList.java
@@ -0,0 +1,11 @@
+public class SLinkedList {
+  protected Node head;
+  protected Node tail;
+  protected long size;
+
+  public SLinkedList() {
+    head = null;
+    tail = null;
+    size = 0;
+  }
+}
