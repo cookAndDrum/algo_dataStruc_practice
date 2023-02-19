4d49198575386792c6b9327ee87586b7d60f2a26
cookAndDrum <99819755+cookAndDrum@users.noreply.github.com>
2023-02-19 21:00:40 +0800

    Update SLinkedList.java

--- dataStruc/SLinkedList.java
+++ dataStruc/SLinkedList.java
@@ -8,4 +8,43 @@ public class SLinkedList {
     tail = null;
     size = 0;
   }
+
+  public void addFirst(Node newNode) {
+    newNode.setNext(head);
+
+    head.setNext(newNode);
+
+    // for C, newNode = null;
+
+    size += 1;
+  }
+
+  public Node removeFirst() {
+    Node temp = head;
+
+    if (temp != null) {
+      head = head.getNext();
+
+      temp.setNext(null);
+
+      size -= 1;
+    }
+
+    return temp;
+  }
+
+  public void addLast(Node newNode) {
+    newNode.setNext(null);
+
+    tail.setNext(newNode);
+
+    tail = newNode;
+
+    size++;
+  }
+
+  public Node removeLast() {
+
+  }
+
 }
